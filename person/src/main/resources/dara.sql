2b7e81f981e4e596de992f87c5108a22fb349f36
Unknown <devilleduc.thomas@gmail.com>
2019-08-01 20:01:26 +0200

    [feat] ajout base de données

--- person/src/main/resources/dara.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO person VALUES(1, 'tdevilleduc', 'Thomas', 'Deville-Duc', 34);
-INSERT INTO person VALUES(2, 'mgianesini', 'Marion', 'Gianesini', 31);
-INSERT INTO person VALUES(3, 'ndanet', 'Nicolas', 'Danet', 27);
---
+++ person/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO person VALUES(1, 34, 'tdevilleduc', 'Thomas', 'Deville-Duc');
+INSERT INTO person VALUES(2, 31, 'mgianesini', 'Marion', 'Gianesini');
+INSERT INTO person VALUES(3, 27, 'ndanet', 'Nicolas', 'Danet');
