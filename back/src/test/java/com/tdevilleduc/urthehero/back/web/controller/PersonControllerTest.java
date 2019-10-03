5786dacf59900f4c57246c9468e6519b4ab6b58b
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2019-10-03 16:50:31 +0200

    [test] add TU of person controller

--- back/src/test/java/com/tdevilleduc/urthehero/back/web/controller/PersonControllerTest.java
+++ back/src/test/java/com/tdevilleduc/urthehero/back/web/controller/PersonControllerTest.java
@@ -1,20 +1,62 @@
 package com.tdevilleduc.urthehero.back.web.controller;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
+@WebAppConfiguration
 public class PersonControllerTest {
 
-    @Test
-    public void testGetPersons() {
-        Assert.assertTrue(true);
+    private static String uriController = "/Person";
+
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
-    public void testGetPersonById() {
-        Assert.assertTrue(true);
+    public void testGetAllPersons() throws Exception {
+        MvcResult resultActions = mockMvc.perform(get(uriController + "/all"))
+                .andExpect(request().asyncStarted())
+                .andReturn();
+        mockMvc.perform(asyncDispatch(resultActions))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(is(notNullValue())));
+    }
+
+    @Test
+    public void testGetPersonById() throws Exception {
+        MvcResult resultActions = mockMvc.perform(get(uriController + "/" + "1"))
+                .andExpect(request().asyncStarted())
+                .andReturn();
+        mockMvc.perform(asyncDispatch(resultActions))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(is(notNullValue())));
     }
 }
---
+++ back/src/test/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO person VALUES(1, 34, 'tdevilleduc', 'Thomas', 'Deville-Duc');
+INSERT INTO person VALUES(2, 31, 'mgianesini', 'Marion', 'Gianesini');
+INSERT INTO person VALUES(3, 27, 'ndanet', 'Nicolas', 'Danet');
+INSERT INTO story VALUES(1, 1, 1, 'Ulysse');
+INSERT INTO story VALUES(2, 2, 4, 'Voyage au bout de la nuit');
+INSERT INTO story VALUES(3, 3, 7, 'Madame Bovary');
+INSERT INTO page VALUES(1, 'image3', 'Ulysse', 1);
+INSERT INTO page VALUES(2, 'image3', 'Dès', 1);
+INSERT INTO page VALUES(3, 'image3', 'Le roman', 1);
+INSERT INTO page VALUES(4, 'image3', 'Voyage au bout de la nuit est le premier roman de Céline, publié en 1932. Ce livre manqua de deux voix le prix Goncourt mais obtient le prix Renaudot1. Il est traduit en 37 langues2.', 2);
+INSERT INTO page VALUES(5, 'image3', 'Le roman est notamment célèbre', 2);
+INSERT INTO page VALUES(6, 'image3', 'Toutefois', 2);
+INSERT INTO page VALUES(7, 'image3', 'Madame Bovary', 3);
