9a0124afea8f12f2ff2fedc4a7aaaeb004e2e09e
Unknown <devilleduc.thomas@gmail.com>
2019-10-04 08:21:13 +0200

    [fix] unit test in personControllerTest

--- back/src/test/java/com/tdevilleduc/urthehero/back/web/controller/PersonControllerTest.java
+++ back/src/test/java/com/tdevilleduc/urthehero/back/web/controller/PersonControllerTest.java
@@ -1,31 +1,33 @@
 package com.tdevilleduc.urthehero.back.web.controller;
 
-import org.junit.Assert;
+import com.tdevilleduc.urthehero.back.BackApplication;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = BackApplication.class)
 @WebAppConfiguration
 public class PersonControllerTest {
 
     private static String uriController = "/Person";
 
+    final String BASE_URL = "http://localhost:8083/";
 
     private MockMvc mockMvc;
 
@@ -40,23 +42,25 @@ public class PersonControllerTest {
 
     @Test
     public void testGetAllPersons() throws Exception {
-        MvcResult resultActions = mockMvc.perform(get(uriController + "/all"))
-                .andExpect(request().asyncStarted())
-                .andReturn();
-        mockMvc.perform(asyncDispatch(resultActions))
+        this.mockMvc.perform(MockMvcRequestBuilders.get(uriController + "/all"))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(is(notNullValue())));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(is(notNullValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)));
     }
 
     @Test
     public void testGetPersonById() throws Exception {
-        MvcResult resultActions = mockMvc.perform(get(uriController + "/" + "1"))
-                .andExpect(request().asyncStarted())
-                .andReturn();
-        mockMvc.perform(asyncDispatch(resultActions))
+        this.mockMvc.perform(MockMvcRequestBuilders.get(uriController + "/2"))
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(is(notNullValue())));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(is(notNullValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.login", Matchers.is("mgianesini")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nom", Matchers.is("Marion")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.prenom", Matchers.is("Gianesini")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age", Matchers.is(31)));
     }
 }
