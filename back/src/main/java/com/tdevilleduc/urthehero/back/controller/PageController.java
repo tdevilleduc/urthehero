e850770edb8a5db561f9600e5087cfd6bc08a4a2
Unknown <devilleduc.thomas@gmail.com>
2019-10-05 22:56:29 +0200

    [feat] add getFirstPageByStory rest entry

--- back/src/main/java/com/tdevilleduc/urthehero/back/controller/PageController.java
+++ back/src/main/java/com/tdevilleduc/urthehero/back/controller/PageController.java
@@ -11,12 +11,17 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @Api( value = "API Page pour les interactions avec les pages" )
 @RestController
+@RequestMapping("/Page")
 public class PageController {
 
     @Autowired
@@ -25,17 +30,15 @@ public class PageController {
     private StoryDao storyDao;
 
     @ApiOperation( value = "Récupère une page à partir de son identifiant" )
-    @GetMapping(value = "/Page/{pageId}")
+    @GetMapping(value = "/{pageId}", produces = APPLICATION_JSON_UTF8_VALUE)
     public Page getPageById(@PathVariable int pageId) {
 
         Page page = pageDao.findById(pageId);
-
         if (page == null) {
             throw new PageNotFoundException("La page avec l'id " + pageId + " n'existe pas");
         }
 
         Story story = page.getStory();
-
         if (story == null) {
             throw new PageNotFoundException("La page avec l'id " + pageId + " n'est pas associée à une histoire");
         }
@@ -44,8 +47,8 @@ public class PageController {
     }
 
     @ApiOperation( value = "Récupère la liste des pages d'une histoire" )
-    @GetMapping(value = "/Pages/{storyId}")
-    public List<Page> getPagesByStoryId(@PathVariable int storyId) {
+    @GetMapping(value = "/all/Story/{storyId}", produces = APPLICATION_JSON_UTF8_VALUE)
+    public List<Page> getAllPagesByStoryId(@PathVariable int storyId) {
 
         Story story = storyDao.findById(storyId);
         if (story == null) {
@@ -53,5 +56,24 @@ public class PageController {
         }
 
         return story.getPages();
+    }
+
+    @ApiOperation( value = "Récupère la première page d'une histoire" )
+    @GetMapping(value = "/Story/{storyId}", produces = APPLICATION_JSON_UTF8_VALUE)
+    public Page getFirstPageByStoryId(@PathVariable int storyId) {
+
+        Story story = storyDao.findById(storyId);
+        if (story == null) {
+            throw new StoryNotFoundException("L'histoire avec l'id " + storyId + " n'existe pas");
+        }
+
+        Integer pageId = story.getFirstPageId();
+
+        Optional<Page> page = pageDao.findById(pageId);
+        if (page.isEmpty()) {
+            throw new PageNotFoundException("La page avec l'id " + pageId + " n'existe pas");
+        }
+
+        return page.get();
     }
 }
