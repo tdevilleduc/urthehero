1e728f66ec90d2342af76a8beb9e19f4a471a10c
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2019-10-09 16:04:26 +0200

    [feat-11] multiple roll of dice has a miscount result

--- back/src/main/java/com/tdevilleduc/urthehero/back/controller/DiceController.java
+++ back/src/main/java/com/tdevilleduc/urthehero/back/controller/DiceController.java
@@ -2,23 +2,18 @@ package com.tdevilleduc.urthehero.back.controller;
 
 import com.tdevilleduc.urthehero.back.model.Dice;
 import com.tdevilleduc.urthehero.back.model.DiceValue;
-import com.tdevilleduc.urthehero.back.model.Page;
 import com.tdevilleduc.urthehero.back.service.IDiceService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.math3.random.RandomDataGenerator;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Stream;
 
 @Api(value = "Dice", tags = { "Dice Controller" } )
 @RestController
@@ -29,19 +24,19 @@ public class DiceController {
     private IDiceService diceService;
 
     @ApiOperation( value = "Effectue un lancer de dés" )
-    @PostMapping(value = "/roll/{dice}")
+    @GetMapping(value = "/roll/{dice}")
     public DiceValue roll(@PathVariable Dice dice) {
         Assert.assertNotNull(dice);
         return diceService.roll(dice);
     }
 
     @ApiOperation( value = "Effectue un lancer de dés avec plusieurs dés en même temps" )
-    @PostMapping(value = "/roll/{dice}/{count}")
+    @GetMapping(value = "/roll/{dice}/{count}")
     public List<DiceValue> roll(@PathVariable Dice dice, @PathVariable Integer count) {
         Assert.assertNotNull(dice);
         Assert.assertNotNull(count);
         List<DiceValue> diceValues = new ArrayList<>();
-        for (int i = 1; i < count; i++) {
+        for (int i = 0; i < count; i++) {
             diceValues.add(diceService.roll(dice));
         }
 
---
+++ back/src/test/java/com/tdevilleduc/urthehero/back/controller/DiceControllerTest.java
@@ -0,0 +1,70 @@
+package com.tdevilleduc.urthehero.back.controller;
+
+import com.tdevilleduc.urthehero.back.BackApplication;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = BackApplication.class)
+@WebAppConfiguration
+public class DiceControllerTest {
+
+
+    private static String uriController = "/Dice";
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void test_rollOne_thenSuccess() throws Exception {
+        String diceString = "DE_20";
+        this.mockMvc.perform(MockMvcRequestBuilders.get(uriController + "/roll/" + diceString))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(is(notNullValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value", Matchers.isA(Integer.class)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value", Matchers.greaterThan(Integer.valueOf(0))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value", Matchers.lessThanOrEqualTo(Integer.valueOf(20))))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dice", Matchers.is(diceString)))
+        ;
+    }
+
+    @Test
+    public void test_rollMulti_thenSuccess() throws Exception {
+        String diceString = "DE_20";
+        Integer numberOfRolls = 4;
+        this.mockMvc.perform(MockMvcRequestBuilders.get(uriController + "/roll/" + diceString + "/" + numberOfRolls))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().string(is(notNullValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(numberOfRolls)))
+        ;
+    }
+}
