25d2bf32a55b9dc5049d48ccb4d8ffdc81e6c3f7
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2019-12-06 15:16:06 +0100

    [feat] remove resilience on story controller
    [feat] add resilience for mysql database

---
+++ back/src/main/java/com/tdevilleduc/urthehero/back/config/ResilienceConfig.java
@@ -0,0 +1,8 @@
+package com.tdevilleduc.urthehero.back.config;
+
+public class ResilienceConfig {
+    public static final String INSTANCE_STORY_SERVICE = "story-service";
+    public static final String INSTANCE_PAGE_SERVICE = "page-service";
+    public static final String INSTANCE_PERSON_SERVICE = "person-service";
+    public static final String INSTANCE_PROGRESSION_SERVICE = "progression-service";
+}
