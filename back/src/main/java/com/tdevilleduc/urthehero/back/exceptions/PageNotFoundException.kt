c95ffa30554152b5b046e8809f5ad20dfa655a4c
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-03-16 14:18:14 +0100

    delete lombok dependency

--- back/src/main/java/com/tdevilleduc/urthehero/back/exceptions/PageNotFoundException.kt
+++ back/src/main/java/com/tdevilleduc/urthehero/back/exceptions/PageNotFoundException.kt
@@ -1,9 +1,7 @@
 package com.tdevilleduc.urthehero.back.exceptions
 
-import lombok.NoArgsConstructor
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@NoArgsConstructor
 @ResponseStatus(HttpStatus.NOT_FOUND)
 class PageNotFoundException(s: String?) : RuntimeException(s)
