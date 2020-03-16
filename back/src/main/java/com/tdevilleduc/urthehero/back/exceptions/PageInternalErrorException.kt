74d4ec6349e520f133fc46053a7a584c00794f8f
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-03-16 14:18:14 +0100

    delete lombok dependency

--- back/src/main/java/com/tdevilleduc/urthehero/back/exceptions/PageInternalErrorException.kt
+++ back/src/main/java/com/tdevilleduc/urthehero/back/exceptions/PageInternalErrorException.kt
@@ -1,9 +1,7 @@
 package com.tdevilleduc.urthehero.back.exceptions
 
-import lombok.NoArgsConstructor
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-@NoArgsConstructor
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 class PageInternalErrorException(s: String?) : RuntimeException(s)
