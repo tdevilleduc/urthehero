52b9ef1e9082b8c57d016234e2b5c1791c90271e
Thomas Deville-Duc <thomas.deville-duc-prestataire@laposte.fr>
2020-03-16 15:34:20 +0100

    [fix] kotlin app cannot start with intellij

--- back/src/main/java/com/tdevilleduc/urthehero/back/BackApplication.kt
+++ back/src/main/java/com/tdevilleduc/urthehero/back/BackApplication.kt
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class BackApplication {
+class BackApplication
 
-    fun main(args: Array<String>) {
-        SpringApplication.run(BackApplication::class.java, *args)
-    }
+fun main(args: Array<String>) {
+    SpringApplication.run(BackApplication::class.java, *args)
 }
