ac1e6663e3e9b277796d342584ba60000efe6fbb
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-04-10 17:38:13 +0200

    [feat-77] add authentication based on login/password from person entity

---
+++ back/src/main/java/com/tdevilleduc/urthehero/back/service/impl/MyUserDetailsService.kt
@@ -0,0 +1,21 @@
+package com.tdevilleduc.urthehero.back.service.impl
+
+import com.tdevilleduc.urthehero.back.service.IPersonService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+
+@Service
+class MyUserDetailsService : UserDetailsService {
+
+    @Autowired
+    private lateinit var personService: IPersonService
+
+    override fun loadUserByUsername(userName: String?): UserDetails {
+        val person = personService.findByLogin(userName!!)
+        return User(person.login, person.password, listOf(SimpleGrantedAuthority("ADMIN")))
+    }
+}
