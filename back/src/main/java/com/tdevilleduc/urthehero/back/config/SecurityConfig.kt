6694c5792720c5bb1ca757f3b96ea0c069966f19
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-04-10 17:38:13 +0200

    [feat-77] add authentication based on login/password from person entity

--- back/src/main/java/com/tdevilleduc/urthehero/back/config/SecurityConfig.kt
+++ back/src/main/java/com/tdevilleduc/urthehero/back/config/SecurityConfig.kt
@@ -1,23 +1,47 @@
 package com.tdevilleduc.urthehero.back.config
 
-import org.springframework.context.annotation.Configuration
+import com.tdevilleduc.urthehero.back.service.impl.MyUserDetailsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
-@Configuration
 @EnableWebSecurity
 internal class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    private lateinit var myUserDetailsService: MyUserDetailsService
+
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
-                .antMatchers(*AUTH_WHITELIST).permitAll()
+                .antMatchers(*AUTHENTICATED_WHITELIST).permitAll()
+                .antMatchers(*AUTHORIZED_WHITELIST).authenticated()
                 .anyRequest().denyAll()
+                .and().formLogin()
+//                .and().httpBasic()
                 .and().csrf().disable()
     }
 
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.userDetailsService(myUserDetailsService)
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return NoOpPasswordEncoder.getInstance()
+    }
+
     companion object {
-        private val AUTH_WHITELIST = arrayOf(
+        private val AUTHENTICATED_WHITELIST = arrayOf(
+                "/login"
+        )
+
+        private val AUTHORIZED_WHITELIST = arrayOf(
                 // -- api requests
                 "/api/**",
                 // -- springDoc requests
